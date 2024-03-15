2f7b782023b26c7278c5ca4983d81c65686b93b8
Deepankar <deepankarsalhan@gmail.com>
2024-03-14 18:00:55 -0600

    serverjs file fixed

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 require('dotenv').config();
 const products = require('./routes/products');
 
-app.use('api/v1/', products);
+app.use('/api/v1/', products);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server port ${process.env.PORT}`);
